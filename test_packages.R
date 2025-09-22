d81c22cd3b56da8b8337ee4cb6795e3d7b3274ba
mathiasbaysmidt <mathias.baysmidt@gmail.com>
2025-09-22 09:26:51 +0000

    Test

--- test_packages.R
+++ test_packages.R
@@ -3,8 +3,3 @@ x = 1
 library(DBI)
 library(tidyverse) # Hello world
 library(RPostgres) # Hi there
-<<<<<<< HEAD
-# Conflict
-=======
-# No conflict
->>>>>>> 062a3f166312b0142891faa5a464b56ab7096818
